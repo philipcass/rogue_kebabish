4cedd9b2863d445fa5d4a04755039300d06a4522
Phil <phil@lappy>
2013-04-01 15:52:48 +0100

    Initial commit

---
+++ Assets/Scripts/BaseFood.cs
@@ -0,0 +1,27 @@
+using System;
+using UnityEngine;
+
+//type-safe-enum pattern
+public class BaseFood:FSprite,ICloneable
+{
+	public int Value{get; set;}
+	public float Speed{get; set;}
+
+    public static readonly BaseFood HAM = new BaseFood ("ham.png",10,1);
+    public static readonly BaseFood STEAK = new BaseFood ("steak.png",10,0.66f);
+    public static readonly BaseFood CHICKEN = new BaseFood ("chicken.png",10,0.33f);        
+
+	
+	public BaseFood (string elementName, int value, float speed):base(elementName)
+	{
+		Value = value;
+		Speed = speed;
+
+	}
+	
+    public object Clone()
+    {
+        return this.MemberwiseClone();
+    }
+
+}
---
+++ Assets/Scripts/BasePlayer.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class BasePlayer : FSprite
+{
+	public BasePlayer (string elementName) : base(elementName)
+	{
+		Futile.instance.SignalUpdate += HandleUpdate;
+	}
+	
+	public void HandleUpdate ()
+	{
+		UpdateMovement();
+	}
+	
+	private void UpdateMovement(){
+		if (this.y < Futile.screen.halfHeight-48)
+			if (Input.GetKey (KeyCode.W))
+				this.y += 4;
+		if (this.y > -Futile.screen.halfHeight+48)
+			if (Input.GetKey (KeyCode.S))
+				this.y -= 4;
+		if (this.x < Futile.screen.halfWidth-48)
+			if (Input.GetKey (KeyCode.D))
+				this.x += 4;
+		if (this.x > -Futile.screen.halfWidth+48)
+			if (Input.GetKey (KeyCode.A))
+				this.x -= 4;
+
+	}
+}
+
