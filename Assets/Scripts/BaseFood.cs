a2c0aa4ff5c74cd2f8628a4262b91d202bd49756
Philip Cass <philipmcass@gmail.com>
2013-04-02 21:55:12 +0100

    Fixed awful auto format. Thanks monodevelop.

--- Assets/Scripts/BaseFood.cs
+++ Assets/Scripts/BaseFood.cs
@@ -8,20 +8,20 @@ public class BaseFood:FSprite,ICloneable
 
     public float Speed{ get; set; }
 
-    public static readonly BaseFood HAM = new BaseFood ("ham.png", 10, 1);
-    public static readonly BaseFood STEAK = new BaseFood ("steak.png", 10, 0.66f);
-    public static readonly BaseFood CHICKEN = new BaseFood ("chicken.png", 10, 0.33f);
+    public static readonly BaseFood HAM = new BaseFood("ham.png", 10, 1);
+    public static readonly BaseFood STEAK = new BaseFood("steak.png", 10, 0.66f);
+    public static readonly BaseFood CHICKEN = new BaseFood("chicken.png", 10, 0.33f);
  
-    public BaseFood (string elementName, int value, float speed):base(elementName)
+    public BaseFood(string elementName, int value, float speed):base(elementName)
     {
         Value = value;
         Speed = speed;
 
     }
  
-    public object Clone ()
+    public object Clone()
     {
-        return this.MemberwiseClone ();
+        return this.MemberwiseClone();
     }
 
 }
--- Assets/Scripts/BasePlayer.cs
+++ Assets/Scripts/BasePlayer.cs
@@ -3,30 +3,30 @@ using UnityEngine;
 
 public class BasePlayer : FSprite
 {
-    public BasePlayer (string elementName) : base(elementName)
+    public BasePlayer(string elementName) : base(elementName)
     {
         Futile.instance.SignalUpdate += HandleUpdate;
     }
  
-    public void HandleUpdate ()
+    public void HandleUpdate()
     {
-        UpdateMovement ();
+        UpdateMovement();
     }
  
-    private void UpdateMovement ()
+    private void UpdateMovement()
     {
-        if (this.y < Futile.screen.halfHeight - 48)
-        if (Input.GetKey (KeyCode.W))
-            this.y += 4;
-        if (this.y > -Futile.screen.halfHeight + 48)
-        if (Input.GetKey (KeyCode.S))
-            this.y -= 4;
-        if (this.x < Futile.screen.halfWidth - 48)
-        if (Input.GetKey (KeyCode.D))
-            this.x += 4;
-        if (this.x > -Futile.screen.halfWidth + 48)
-        if (Input.GetKey (KeyCode.A))
-            this.x -= 4;
+        if(this.y < Futile.screen.halfHeight - 48)
+            if(Input.GetKey(KeyCode.W))
+                this.y += 4;
+        if(this.y > -Futile.screen.halfHeight + 48)
+            if(Input.GetKey(KeyCode.S))
+                this.y -= 4;
+        if(this.x < Futile.screen.halfWidth - 48)
+            if(Input.GetKey(KeyCode.D))
+                this.x += 4;
+        if(this.x > -Futile.screen.halfWidth + 48)
+            if(Input.GetKey(KeyCode.A))
+                this.x -= 4;
 
     }
 }
